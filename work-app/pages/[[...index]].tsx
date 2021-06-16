b2be6379a474362b9d1a6aa01b3aca9d460aa8a4
aqibgatoo <mohd.aqib.gatoo@axelerant.com>
2021-06-16 17:48:25 +0530

    Add protection to root component

--- work-app/pages/[[...index]].tsx
+++ work-app/pages/[[...index]].tsx
@@ -1,7 +1,17 @@
 import { getEntitiesFromContext } from "next-drupal";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useAuth } from "../src/auth/AuthContext";
 import { RootComponent } from "../src/components";
 import { Layout } from "../src/layout";
 const Index = ({ data }) => {
+  const { push } = useRouter();
+  const { user, login, logout } = useAuth();
+  useEffect(() => {
+    if (!user) {
+      push("/login");
+    }
+  }, [user]);
   return (
     <Layout>
       <RootComponent data={data} />
